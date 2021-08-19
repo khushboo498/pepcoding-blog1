82db866453e03da6caf65f2db5c5deca8261d609
Khushboo Singh <singhkhushboo498@gmail.com>
2021-08-19 17:43:21 +0530

    script tag added and media querry

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+ 	$("#menu-button").click(function(){
+    	$("#small-screen-nav").toggle();
+ 	});
+});
+
+
+// var flag = 0;
+
+// function navBar(){
+// 	if(flag === 0){
+// 		document.getElementsByClassName('small-scree-nav').style.display = 'block';		
+// 	}
+// 	else{
+		
+// 		document.getElementsByClassName('small-scree-nav').style.display = 'none';
+// 	}
+// 	flag = !flag
+// }
+
+
